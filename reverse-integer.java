6fddef2e7ffb597b4694ea9dbf7073ef93239d5e
Artem <79024745+aseratonin@users.noreply.github.com>
2021-08-05 15:17:55 -0500

    Update reverse-integer.java

--- reverse-integer.java
+++ reverse-integer.java
@@ -1,16 +1,15 @@
 class Solution {
     public int reverse(int x) {
-        // initialize rev variable for storage reverse number
         int rev = 0;
-        
         while ( x != 0) {
-        // do division while number not 0
-            int reverse = x % 10;
-            //updating rev variable with turned number 
-            rev = rev * 10 + reverse;
-            x /= 10;
+            int res = x % 10;
+            int newResult = rev * 10 + res;
+            if ((newResult - res) / 10 != rev)
+            {return 0;}
+            rev = newResult;
+            x = x / 10;
         }
-        
+
         return rev;
     }
 }
